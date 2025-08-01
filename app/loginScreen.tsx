e105585e98fd6b6abd4cd6e74c23838eb6ce1c97
Md Mohiuddin Murad <murad.stepup@gmail.com>
2025-08-01 15:59:53 +0600

    Set the route and add login screen

---
+++ app/loginScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native'
+
+
+export default function LoginScreen() {
+    return (
+        <View>
+            <Text style={{marginTop: 10, matginRight: 20}}>Login Screen</Text>
+        </View>
+    )
+}
